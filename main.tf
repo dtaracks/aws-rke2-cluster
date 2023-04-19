3affc397aa2e871a8a3f85abc3b64c2f29a975d1
Drew Thomson <drew.thomson@sysdig.com>
2023-04-19 13:27:17 -0500

    remove old comments from main.tf

--- main.tf
+++ main.tf
@@ -22,15 +22,6 @@ resource "aws_instance" "masters" {
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   key_name = "${var.key_name}"
 
-  #tags = {
-  #  Owner = "drew.thomson"
-  #  Approver = "robert.mendieta"
-  #  Purpose = "install.testing"
-  #  Description = "install.testing"
-  #  expirationDate = "2023-04-01"
-  #  Terraform = "true"
-  #}
-
   root_block_device {
     volume_size = var.volume_size
     volume_type = var.volume_type
@@ -49,15 +40,6 @@ resource "aws_instance" "workers" {
   subnet_id = module.vpc.public_subnets[0]
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   key_name = "${var.key_name}"
-
-  #tags = {
-  #  Owner = "drew.thomson"
-  #  Approver = "robert.mendieta"
-  #  Purpose = "install.testing"
-  #  Description = "install.testing"
-  #  expirationDate = "2023-04-01"
-  #  Terraform = "true"
-  #}
 
   root_block_device {
     volume_size = var.volume_size
