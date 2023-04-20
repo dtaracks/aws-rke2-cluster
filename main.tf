bef1f1d2592186fadbe52e7bfd0bb79894f7aba4
danial.taracks <danial.taracks@sysdig.com>
2023-04-20 18:27:19 +0000

    updated wrker tag to worker

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "workers" {
   key_name = "${var.key_name}"
 
   tags = {
-    Name = "wrker-${var.instance_name_prefix}-${random_string.suffix.result}"
+    Name = "worker-${var.instance_name_prefix}-${random_string.suffix.result}"
   }
 
   root_block_device {
